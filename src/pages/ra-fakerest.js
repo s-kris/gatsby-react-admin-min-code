457d03e6baf04a5f25d2d71f8624ecc0fd6778a1
s-kris <skris4291@gmail.com>
2020-03-04 16:59:31 +0530

    add fakerest data provider

--- src/pages/ra-fakerest.js
+++ src/pages/ra-fakerest.js
@@ -6,16 +6,26 @@ import { Switch, Route } from 'react-router-dom'
 import withContext from 'recompose/withContext'
 import { AuthContext, DataProviderContext, Resource, List, Datagrid, TextField } from 'react-admin'
 import { ThemeProvider } from '@material-ui/styles'
-import restProvider from 'ra-data-simple-rest'
-import { createMuiTheme } from '@material-ui/core';
+import { createMuiTheme } from '@material-ui/core'
+import fakeDataProvider from 'ra-data-fakerest'
 
-import Layout from './../components/layout'
-import Dashboard from './../components/dashboard'
-import createAdminStore from './../createAdminStore'
+import Layout from '../components/layout'
+import Dashboard from '../components/dashboard'
+import createAdminStore from '../createAdminStore'
 
 
-// dependency injection
-const dataProvider = restProvider('http://path.to.my.api/')
+const dataProvider = fakeDataProvider({
+    posts: [
+        { id: 0, title: 'Hello, world!' },
+        { id: 1, title: 'FooBar' },
+    ],
+    comments: [
+        { id: 0, post_id: 0, author: 'John Doe', body: 'Sensational!' },
+        { id: 1, post_id: 0, author: 'Jane Doe', body: 'I agree' },
+    ],
+})
+
+
 const authProvider = () => Promise.resolve()
 const history = createHashHistory()
 const theme = createMuiTheme({
@@ -35,16 +45,16 @@ const theme = createMuiTheme({
             },
         },
     },
-});
+})
 
 const PostList = props => (
     <List {...props}>
         <Datagrid>
             <TextField source="id" />
-            <TextField source="name" />
+            <TextField source="title" />
         </Datagrid>
     </List>
-);
+)
 
 const ReactAdminPage = () => {
     return <Layout>react-admin-min-code
